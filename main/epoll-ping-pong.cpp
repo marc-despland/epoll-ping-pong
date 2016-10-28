77fa962e58b4c2f1b9698fca1bba0e1053071be0
Marc Despland <marc.despland@art122-5.net>
2016-10-28 11:46:34 +0200

    dummy project

---
+++ main/epoll-ping-pong.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "options.h"
+#include "log.h"
+
+
+
+int main(int argc, char **argv) {
+	Log::logger->setLevel(ERROR);
+	Options options(argv[0], "1.0.0", "Epoll Ping Pong https://github.com/marc-despland/epoll-ping-pong");
+	try {
+		options.add('d', "debug", "Start on debug mode", false, false);
+	} catch(ExistingOptionException &e ) {
+	}
+	try {
+		options.parse(argc, argv);
+		if (options.get('d')->isAssign()) Log::logger->setLevel(DEBUG);
+		
+	} catch (OptionsStopException &e) {
+	} catch (UnknownOptionException &e) {
+		cout << " Request unknown option"<<endl;
+	}
+
+}
